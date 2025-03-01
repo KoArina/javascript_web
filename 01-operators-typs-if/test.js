e5d93263f5bd5b907051be7da92a270b8dd03d5a
Arina <arina@arina.ua>
2025-03-01 12:02:11 +0200

    lesson 1

---
+++ 01-operators-typs-if/test.js
@@ -0,0 +1,20 @@
+// коментар на 1 рядок
+
+/*
+коментар
+на
+багато
+рядків
+*/
+
+// змінні
+let myVariable = 12;
+let number = 3.33;
+let text = "тут текст";
+
+const MY_NAME = "arina";
+let my1 = 10;
+let My1 = 10;
+
+myVariable = number + myVariable
+
